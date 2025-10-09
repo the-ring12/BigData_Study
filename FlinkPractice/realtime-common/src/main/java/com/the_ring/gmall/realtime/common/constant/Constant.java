0ca43ffa68019f6a9cfb9c73503639b090bf57a5
the_ring <the_ring1@163.com>
2025-10-09 16:29:21 +0800

    常量类

---
+++ FlinkPractice/realtime-common/src/main/java/com/the_ring/gmall/realtime/common/constant/Constant.java
@@ -0,0 +1,39 @@
+package com.the_ring.gmall.realtime.common.constant;
+
+/**
+ * @Description 常量类
+ * @Date 2025/10/9
+ * @Author the_ring
+ */
+public class Constant {
+
+    public static final String KAFKA_BROKERS = "hadoop00:9092,hadoop01:9092,hadoop02:9092";
+    public static final String TOPIC_DB = "topic-db";
+    public static final String TOPIC_LOG = "topic-log";
+
+    public static final String MYSQL_HOST = "hadoop00";
+    public static final int MYSQL_PORT = 3306;
+    public static final String MYSQL_USER_NAME = "root";
+    public static final String MYSQL_PASSWORD = "Mysql123456";
+    public static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String MYSQL_URL = "jdbc://hadoop100:3306?useSSL=false";
+
+    public static final String HBASE_NAMESPACE = "gmall";
+
+    public static final String TOPIC_DWD_TRAFFIC_START = "dwd_traffic_start";
+    public static final String TOPIC_DWD_TRAFFIC_ERR = "dwd_traffic_err";
+    public static final String TOPIC_DWD_TRAFFIC_PAGE = "dwd_traffic_page";
+    public static final String TOPIC_DWD_TRAFFIC_ACTION = "dwd_traffic_action";
+    public static final String TOPIC_DWD_TRAFFIC_DISPLAY = "dwd_traffic_display";
+
+    public static final String TOPIC_DWD_INTERACTION_COMMENT_INFO = "dwd_interaction_comment_info";
+
+    public static final String TOPIC_DWD_TRADE_CART_ADD = "dwd_trade_cart_add";
+    public static final String TOPIC_DWD_TRADE_ORDER_DETAIL = "dwd_trade_order_detail";
+    public static final String TOPIC_DWD_TRADE_ORDER_CANCEL = "dwd_trade_order_cancel";
+    public static final String TOPIC_DWD_TRADE_ORDER_PAYMENT_SUCCESS = "dwd_trade_order_payment_success";
+    public static final String TOPIC_DWD_TRADE_ORDER_REFUND = "dwd_trade_order_refund";
+    public static final String TOPIC_DWD_TRADE_REFUND_PAYMENT_SUCCESS = "dwd_trade_refund_payment_success";
+
+    public static final String TOPIC_DWD_USER_REGISTER = "dwd_user_register";
+}
